09e0940b9efb9112e0e91641b137f64ba036531c
Sandra <sandra@bullok>
2023-08-11 18:10:30 +0300

    improve: code

--- src/main/App/utils.ts
+++ src/main/App/utils.ts
@@ -1,6 +1,13 @@
-import { writeFile } from 'fs/promises';
-import { buildPathFromRoot } from '../../utils/helpers';
 import { existsSync } from 'fs';
+import { writeFile } from 'fs/promises';
+import { initRenderersErrorListeners } from '../../shared/Errors/renderersErrorListeners';
+import { createErrorLogFolder } from '../../shared/Errors/utils';
+import { initWindowInstanceRegistry } from '../../shared/windowRegistries/utils';
+import { buildPathFromRoot } from '../../utils/helpers';
+import { getAppSettingsData } from '../DB/utils';
+import { initWebDBListeners } from '../DB/webDBListeners';
+import { createMainWindow } from '../mainWindow/windowCreation';
+import { initPassivePractice } from '../practice/passive/main';
 
 const APP_CONFIG = {
   firstLaunch: false
@@ -11,3 +18,53 @@ export const createAppConfig = () => {
 };
 
 export const checkAppFirstLaunch = () => existsSync(buildPathFromRoot("appConfig.json")) ? false : true;
+
+/**
+ * runs before app ready 
+ */
+export const appLaunchPreparer = async () => {
+  let openAppWindowAtStart: boolean;
+
+  if (checkAppFirstLaunch()) {
+    openAppWindowAtStart = true;
+    appFirstLaunchSetup();
+  } else {
+    ({ openAppWindowAtStart } = (await getAppSettingsData()).basic);
+    appNotFirstLaunchSetup();
+  }
+
+  appGeneralSetup();
+
+  return openAppWindowAtStart;
+};
+
+export const appFirstLaunchSetup = () => {
+  createAppConfig();
+};
+
+export const appNotFirstLaunchSetup = () => {
+  initPassivePractice();
+};
+
+/**
+ * runs anyway on app start
+ */
+export const appGeneralSetup = () => {
+  createErrorLogFolder();
+  initRenderersErrorListeners();
+  initWindowInstanceRegistry();
+  initWebDBListeners();
+};
+
+type OnAppReadyProps = {
+  openAppWindowAtStart: boolean;
+};
+
+/**
+ * runs after app ready
+ */
+export const onAppReady = (data: OnAppReadyProps) => {
+  if (data.openAppWindowAtStart) {
+    createMainWindow();
+  }
+};
--- src/main/index.ts
+++ src/main/index.ts
@@ -1,36 +1,10 @@
-import { initRenderersErrorListeners } from '../shared/Errors/renderersErrorListeners';
-import { createErrorLogFolder } from '../shared/Errors/utils';
-import { initWindowInstanceRegistry } from '../shared/windowRegistries/utils';
 import { launchApp } from './App/launchApp';
-import { checkAppFirstLaunch, createAppConfig } from './App/utils';
-import { getAppSettingsData } from './DB/utils';
-import { initWebDBListeners } from './DB/webDBListeners';
-import { createMainWindow } from './mainWindow/windowCreation';
-import { initPassivePractice } from './practice/passive/main';
+import { appLaunchPreparer as appLaunchPreparer, onAppReady } from './App/utils';
 
 (async () => {
-  let openAppWindowAtStart: boolean;
+  const openAppWindowAtStart = await appLaunchPreparer();
 
-  if (checkAppFirstLaunch()) {
-    createAppConfig();
-    openAppWindowAtStart = true;
-  } else {
-    ({ openAppWindowAtStart } = (await getAppSettingsData()).basic);
-    initPassivePractice();
-  }
+  await launchApp();
 
-  createErrorLogFolder();
-  initRenderersErrorListeners();
-  initWindowInstanceRegistry();
-  initWebDBListeners();
-
-  launchApp({
-    afterLaunch: [
-      () => {
-        openAppWindowAtStart && createMainWindow();
-        // createMainWindow();
-
-      }
-    ]
-  });
+  onAppReady({ openAppWindowAtStart });
 })();
