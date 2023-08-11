998955cbedda738a5b046eed469d448ddf6f8f65
Sandra <sandra@bullok>
2023-08-11 09:54:46 +0300

    improve: code

--- src/shared/ipcMain/renderersErrorListeners.ts
+++ src/shared/ipcMain/renderersErrorListeners.ts
@@ -4,13 +4,13 @@ import { join } from 'path';
 import { ERRORS_LOGS_FOLDER, writeIpcMainError } from '../../utils/helpers';
 
 export const initRenderersErrorListeners = () => {
-  ipcMain.on("mainWinErr", async (_, err: string) => {
-    appendFile(join(ERRORS_LOGS_FOLDER, "mainWinErrs.txt"), `${String(new Date().toDateString())} - ${err}\n`)
+  ipcMain.on("mainWinErr", async (_, errMsg: string) => {
+    appendFile(join(ERRORS_LOGS_FOLDER, "mainWinErrs.txt"), `${String(new Date().toDateString())} - ${errMsg}\n`)
       .catch((err) => writeIpcMainError(err));
   });
 
-  ipcMain.on("passivePracticeWinErr", async (_, err: string) => {
-    appendFile(join(ERRORS_LOGS_FOLDER, "passivePracticeWinErr.txt"), `${String(new Date().toDateString())} - ${err}\n`)
+  ipcMain.on("passivePracticeWinErr", async (_, errMsg: string) => {
+    appendFile(join(ERRORS_LOGS_FOLDER, "passivePracticeWinErr.txt"), `${String(new Date().toDateString())} - ${errMsg}\n`)
       .catch((err) => writeIpcMainError(err));
   });
 };
