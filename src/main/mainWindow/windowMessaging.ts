d752cc15a8018d9134d2390c494a785f5150d4fa
Sandra <sandra@bullok>
2023-08-19 21:04:19 +0300

    fix: statistic update on passivePractice

--- src/main/mainWindow/windowMessaging.ts
+++ src/main/mainWindow/windowMessaging.ts
@@ -1,6 +1,6 @@
 import { windowInstanceRegistry } from '../../shared/windowRegistries/windowInstanceRegistry';
 import { Word } from '../DB/interface';
 
-export const sendToMainWindowNewPassivePracticeResult = (result: boolean, word: Word) => {
-  windowInstanceRegistry.get("main")!.getWin()!.webContents.send("newPassivePracticeComplete", result, word.id);
+export const sendToMainWindowNewPassivePracticeResult = (result: boolean, word: Word, practiceTarget: "original" | "translation") => {
+  windowInstanceRegistry.get("main")!.getWin()!.webContents.send("newPassivePracticeComplete", result, word.id, practiceTarget);
 };
--- src/main/practice/passive/windowListeners.ts
+++ src/main/practice/passive/windowListeners.ts
@@ -1,21 +1,22 @@
 import { ipcMain } from 'electron';
 import { windowInstanceRegistry } from '../../../shared/windowRegistries/windowInstanceRegistry';
-import { Word } from '../../DB/interface';
+import { Word, YearData } from '../../DB/interface';
 import { utilsWithCatch } from '../../DB/utilsWithCatch';
 import { sendToMainWindowNewPassivePracticeResult } from '../../mainWindow/windowMessaging';
 
-const onPassivePracticeResult = (e: Electron.IpcMainEvent, result: boolean, word: Word) => {
+const onPassivePracticeResult = (e: Electron.IpcMainEvent, result: boolean, word: Word, practiceTarget: "original" | "translation") => {
   if (windowInstanceRegistry.get("main")!.getIsClosed()) {
     e.reply("updateDBPracticeResult");
   } else {
     windowInstanceRegistry.get("passivePractice")!.close();
-    sendToMainWindowNewPassivePracticeResult(result, word);
+    sendToMainWindowNewPassivePracticeResult(result, word, practiceTarget);
   }
 };
 
 // if main win is closed - update by passivePractice win
-const onUpdateDBWithPracticeResult = (_, word: Word) => {
-  utilsWithCatch.get("onAddWord")!(word);
+const onUpdateDBWithPracticeResult = (_, updatedWord: Word, updatedCurYearData: YearData) => {
+  utilsWithCatch.get("onAddWord")!(updatedWord);
+  utilsWithCatch.get("updateStatistic")!(updatedCurYearData);
   windowInstanceRegistry.get("passivePractice")!.close();
 };
 
@@ -24,6 +25,6 @@ const onUpdateDBWithPracticeResult = (_, word: Word) => {
  */
 export const initWindowListeners = () => {
   ipcMain.on("passivePracticeResult", onPassivePracticeResult);
-  // if main win is closed - update by passivePractice win
+  // if main win is closed - update by sended data from passivePractice win
   ipcMain.on("updateDBPracticeResult", onUpdateDBWithPracticeResult);
 };
