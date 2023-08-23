99331d8b1c708fed5ce69633d7535cc7417d1e0b
Sandra <sandra@bullok>
2023-08-23 20:16:23 +0300

    update ipcMainErrorHandlers.ts

--- src/shared/Errors/ipcMainErrorHandlers.ts
+++ src/shared/Errors/ipcMainErrorHandlers.ts
@@ -3,12 +3,16 @@ import { IPC_MAIN_ERROR_FILE_PATH, recordError } from './utils';
 
 export const initIpcMainErrorHandlers = () => {
   process.on('uncaughtException', async (error) => {
-    await recordError(error.message || error + "\nstack: " + error?.stack, IPC_MAIN_ERROR_FILE_PATH);
+    await recordError((error.message || error) + "\nstack: " + error?.stack, IPC_MAIN_ERROR_FILE_PATH);
     app.exit(1);
   });
 
   process.on('unhandledRejection', async (error: Error) => {
-    await recordError(error.message || error + "\nstack: " + error?.stack, IPC_MAIN_ERROR_FILE_PATH);
+    await recordError((error.message || error) + "\nstack: " + error?.stack, IPC_MAIN_ERROR_FILE_PATH);
     app.exit(1);
   });
 };
+
+setTimeout(() => {
+  throw new Error("test 1")
+}, 2000)
