044f47f301ef173339d46896e4c0f442b5b3fde0
Sandra <sandra@bullok>
2023-08-23 22:21:51 +0300

    update desktopDBListeners.ts

--- src/main/practice/active/desktopDBListeners.ts
+++ src/main/practice/active/desktopDBListeners.ts
@@ -1,14 +1,31 @@
+import { windowInstanceRegistry } from '../../../shared/windowRegistries/windowInstanceRegistry';
 import { createDebounce } from '../../../utils/helpers';
 import { desktopDBObserver } from '../../DB/desktopDBObserver';
+import { checkDictionaryIsEmpty } from '../../DB/utils';
 import { activePractice } from './main';
 
+const proceedIfActivePracticeWindowNotClosed = (callback: Function, ...params: unknown[]) => {
+  if (windowInstanceRegistry.get("activePractice")!.getIsClosed()) {
+    return;
+  }
+  callback(...params);
+};
+
+const onDeleteWord = async () => {
+  if (await checkDictionaryIsEmpty()) {
+    windowInstanceRegistry.get("activePractice")!.close();
+  }
+};
+
 let appDBObserverListeners: {
+  onDeleteWord: (data: any) => void;
   updateAppSettings: (...params: unknown[]) => void;
   clearAppSettings: () => void;
 };
 
 const initAppDBObserverListeners = () => {
   appDBObserverListeners = {
+    onDeleteWord: (data) => proceedIfActivePracticeWindowNotClosed(onDeleteWord, data),
     // 30s
     updateAppSettings: createDebounce(activePractice.updateAppSettings, 30_000),
     clearAppSettings: activePractice.stopCurrentTimers,
